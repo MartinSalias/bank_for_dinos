f3dff7e9badd54c47d20af571f9e8c6e4230ce23
luis petek <lmpetek@gmail.com>
2015-11-07 14:40:08 -0300

    add custom validators method

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,8 +1,22 @@
+class MyValidator < ActiveModel::Validator
+  def validate(record)
+    if (record.sign.to_i < 0 && record.amount > 0) || (record.sign.to_i > 0 && record.amount < 0)
+      record.errors[:amount] << 'Incorrect amount!'
+    end
+  end
+end
+
 class Transaction < ActiveRecord::Base
   belongs_to :account
   #validates :amount, :numericality => {:greater_than => 0}
+  validate :validate_amount
+  #validates_with MyValidator
 
   attr_accessor :sign
 
-
+  def validate_amount
+    if (sign.to_i < 0 && amount > 0) || (sign.to_i > 0 && amount < 0)
+      errors.add(:amount,'Incorrect amount!')
+    end
+  end
 end
